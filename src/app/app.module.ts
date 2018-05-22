bd373729c8a838d6616e7d3abb9dd55a5d658626
prabhash <pmgit@skysoft.net.in>
2018-05-22 23:53:44 +0530

    added 2 elements on same page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 
 import { ButtonComponent } from './button/button.component';
+import { Button2Component } from './button2/button2.component';
 
 @NgModule({
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, Button2Component],
   imports: [BrowserModule],
-  entryComponents: [ButtonComponent]
+  entryComponents: [ButtonComponent,Button2Component]
 })
 export class AppModule {
   constructor(private injector: Injector) {
     const customButton = createCustomElement(ButtonComponent, { injector });
     customElements.define('custom-button', customButton);
+    const customButton2 = createCustomElement(Button2Component, { injector });
+    customElements.define('custom-button2', customButton2);
   }
 
   ngDoBootstrap() {}
--- src/app/button2/button2.component.ts
+++ src/app/button2/button2.component.ts
@@ -7,21 +7,21 @@ import {
 } from '@angular/core';
 
 @Component({
-  selector: 'custom-button',
-  template: `<button (click)="handleClick()">{{label}}</button>`,
+  selector: 'custom-button2',
+  template: `<button (click)="handleClick()">Hi 2 {{label}}</button>`,
   styles: [
     `
     button {
       border: solid 3px;
       padding: 8px 10px;
-      background: red;
+      background: green;
       font-size: 30px;
     }
   `
   ],
   encapsulation: ViewEncapsulation.Native
 })
-export class ButtonComponent {
+export class Button2Component {
   @Input() label = 'default label';
   @Output() action = new EventEmitter<number>();
   private clicksCt = 0;
